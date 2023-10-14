eec72f87cf853e89c122a450e915c57e19cb11c3
Mykola Fedotov <fedotovns@gmail.com>
2023-10-14 20:44:07 +0200

    task09 -done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  btn: document.querySelector(".change-color"),
+  hexCode: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
+refs.btn.addEventListener("click", onBtnClick);
+
+function onBtnClick(e) {
+  const color = getRandomHexColor();
+  return (
+    (refs.hexCode.textContent = color) &&
+    (refs.body.style.backgroundColor = color)
+  );
+}
